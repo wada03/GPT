d5082134c07cd2e0c67d108771330ae3f32cc52f
WADA Tatsunori <121793910+wada03@users.noreply.github.com>
2025-06-05 20:40:07 +0900

    Create template.py

---
+++ template.py
@@ -0,0 +1,39 @@
+from pygame import *
+from settings import *
+
+# 設定
+init()
+screen = display.set_mode((WIDTH, HEIGHT))
+clock  = time.Clock()
+
+# 定義(クラス・関数)
+
+# 準備(スプライト・マップ・変数)
+all_sprites = sprite.Group()
+{SPRITE_GROUPS}
+
+# メインループ
+run = True
+while run:
+    # イベント
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        {EVENT_ACTIONS}
+
+    # 更新
+    all_sprites.update()
+
+    # 判定
+    {COLLISIONS} 
+    {GAME_ACTIONS}  
+
+    # 描画
+    screen.fill({BG})
+    all_sprites.draw(screen)
+    {UI_DRAW}  
+    display.flip()
+
+    clock.tick(FPS)
+
+quit()
